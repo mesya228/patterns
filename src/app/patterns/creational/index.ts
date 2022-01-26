295859a5cb0090f6467e10c494f50057942f5d84
mesya <mesya13@gmail.com>
2022-01-26 21:22:02 +0200

    feat: moved patters to creational folder;

---
+++ src/app/patterns/creational/index.ts
@@ -0,0 +1,5 @@
+export { SingletonComponent } from './5-singleton/singleton.component';
+export { PrototypeComponent } from './4-prototype/prototype.component';
+export { BuilderComponent } from './3-builder/builder.component';
+export { AbstractFactoryComponent } from './2-abstract-factory/abstract-factory.component';
+export { FactoryComponent } from './1-factory/factory.component';
